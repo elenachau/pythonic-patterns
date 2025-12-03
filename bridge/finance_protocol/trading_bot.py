c6a20861bd8e89f345fc3cb5a6d8503a3df0da39
elenachau <elenaxcha@gmail.com>
2025-12-02 17:26:39 -0800

    cleanup

--- bridge/finance_protocol/trading_bot.py
+++ bridge/finance_protocol/trading_bot.py
@@ -2,8 +2,8 @@ from typing import Protocol
 
 
 class TradingBot(Protocol):
-    def should_buy(self, symbol: str) -> bool:
+    def should_buy(self, symbol: str):
         """Should the bot buy the given symbol?"""
 
-    def should_sell(self, symbol: str) -> bool:
+    def should_sell(self, symbol: str):
         """Should the bot sell the given symbol?"""
