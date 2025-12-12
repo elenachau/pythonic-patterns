cc247b36e1e51411ab7dff47c7d5c72c7353e1fd
elenachau <elenaxcha@gmail.com>
2025-12-12 09:09:23 -0800

    remove unused import

--- notification_patterns/mediator.py
+++ notification_patterns/mediator.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from pydoc import text
 from typing import Optional
 
 class Mediator(ABC):
