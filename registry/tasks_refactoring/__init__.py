f00ea5ff8d5bb67c5030bffae4a81e09d6d57ab2
elenachau <elenaxcha@gmail.com>
2025-12-14 11:28:42 -0800

    create class based registry pattern

---
+++ registry/tasks_refactoring/__init__.py
@@ -0,0 +1,11 @@
+# """Task registry refactoring example."""
+
+# from .registry import TaskRegistry
+# from .tasks import PulseFactory, RecalibrateFactory, ReinforceFactory
+
+# __all__ = [
+#     "TaskRegistry",
+#     "PulseFactory",
+#     "RecalibrateFactory",
+#     "ReinforceFactory",
+# ]
